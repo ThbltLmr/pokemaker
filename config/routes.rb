3fa1b174607159d3a58304147afbeaa24209d1ef
Kabol Amine <amine.kabol@gmail.com>
2023-06-08 12:46:29 +0200

    pull origin master

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
+  get '/home_map', to: 'pages#home_map'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
