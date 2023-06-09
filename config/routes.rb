fb93b3811fa546bc34d66d47ad230e01cfcb5693
Kabol Amine <amine.kabol@gmail.com>
2023-06-09 14:08:06 +0200

    transition home page done manque à régler le problème avec le player

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   resources :pokemons, only: [:new, :create, :index]
   root "pages#home"
-  get '/map', to: 'pages#map'
   get "/pokemons_new", to: "pokemons#new"
   get "/pokemons_index", to: "pokemons#index"
   get "/profile", to: "pages#profile"
