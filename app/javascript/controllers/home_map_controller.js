a582a89b4aa64ea522f5dff5b3c1f15d32bd37fd
Kabol Amine <amine.kabol@gmail.com>
2023-06-09 14:53:57 +0200

    mouvement player map done avec fake redirection

--- app/javascript/controllers/home_map_controller.js
+++ app/javascript/controllers/home_map_controller.js
@@ -14,35 +14,60 @@ export default class extends Controller {
         y: 0
     }
 
+    const backgroundWidth = 1600;
+    const backgroundHeight = 1000;
+
     function run(){
         player_pos.x += player_move.x
         player_pos.y += player_move.y
+
+        if (player_pos.x < 0) {
+          player_pos.x = 0;
+        }
+        if (player_pos.x > backgroundWidth - player.offsetWidth) {
+          player_pos.x = backgroundWidth - player.offsetWidth;
+        }
+        if (player_pos.y < 0) {
+          player_pos.y = 0;
+        }
+        if (player_pos.y > backgroundHeight - player.offsetHeight) {
+          player_pos.y = backgroundHeight - player.offsetHeight;
+        }
 
         player.style.left = player_pos.x + 'px'
         player.style.bottom = player_pos.y + 'px'
 
         requestAnimationFrame(run)
     }
+     run()
 
     window.addEventListener('keydown', function(e){
         if(e.key == "ArrowUp"){
             player_move.y = 3
-            player.style.backgroundImage = 'url("image/player_front.png")'
+            player.classList.add('player-front')
+            player.classList.remove('player-back', 'player-left', 'player-right')
         }
 
         if(e.key == "ArrowDown"){
             player_move.y = -3
-            player.style.backgroundImage = 'url("image/player_back.png")'
+            player.classList.add('player-back')
+            player.classList.remove('player-front', 'player-left', 'player-right')
         }
         if(e.key == "ArrowLeft"){
             player_move.x = -3
-            player.style.backgroundImage = 'url("image/player_left.png")'
+            player.classList.add('player-left')
+            player.classList.remove('player-back', 'player-front', 'player-right')
         }
         if(e.key == "ArrowRight"){
             player_move.x = 3
-            player.style.backgroundImage = 'url("image/player_right.png")'
+            player.classList.add('player-right')
+            player.classList.remove('player-back', 'player-left', 'player-front')
         }
         player.classList.add('active')
+        if(e.key == "a") {
+          window.location = 'http://www.google.com'
+        }
+
     })
 
     window.addEventListener('keyup', function(){
