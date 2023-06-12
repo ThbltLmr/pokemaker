51756faae433cfb1c6c3b8a793fc1dbedc5a0040
Kabol Amine <amine.kabol@gmail.com>
2023-06-12 17:23:23 +0200

    retravail de la home page et signup/signin pimpé

--- app/javascript/controllers/home_controller.js
+++ app/javascript/controllers/home_controller.js
@@ -3,7 +3,6 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="home"
 export default class extends Controller {
   reveal(evt) {
-    console.log('hello from home controller')
     evt.preventDefault();
     evt.stopPropagation();
 
