3a819b878ef758d6df5ea269b27143759c3a1359
Thibault Lemery <lemery.thibault.pro@gmail.com>
2023-06-19 09:45:24 +0200

    done

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -28,28 +28,31 @@ class PokemonsController < ApplicationController
   end
 
   def create
-    @pokemon = Pokemon.new(pokemon_params)
-    @pokemon.user = current_user
+    if current_user.pokemons.count >= 6
+      redirect_to root_path, alert: "You have already created the maximum number of pokemons allowed"
+    else
+      @pokemon = Pokemon.new(pokemon_params)
+      @pokemon.user = current_user
 
-    # debugger
-    if @pokemon.valid?
-      if @pokemon.last_step?
-        MidJourneyResult.new(@pokemon).call
-        if @pokemon.photo.attached?
-          @pokemon.save
-          @attacks = params.dig(:pokemon, :attacks).split
-          create_types(@pokemon, params.dig(:pokemon, :type_ids))
-          create_attacks(@pokemon, @attacks)
-          render json: { html: reveal(@pokemon) }
+      if @pokemon.valid?
+        if @pokemon.last_step?
+          MidJourneyResult.new(@pokemon).call
+          if @pokemon.photo.attached?
+            @pokemon.save
+            @attacks = params.dig(:pokemon, :attacks).split
+            create_types(@pokemon, params.dig(:pokemon, :type_ids))
+            create_attacks(@pokemon, @attacks)
+            render json: { html: reveal(@pokemon) }
+          else
+            render json: { html: loading }
+          end
         else
-          render json: { html: loading }
+          @pokemon.next_step!
+          render json: { html: partial }
         end
       else
-        @pokemon.next_step!
-        render json: { html: partial }
+        render json: { html: partial }, status: :unprocessable_entity
       end
-    else
-      render json: { html: partial }, status: :unprocessable_entity
     end
   end
 
@@ -88,7 +91,7 @@ class PokemonsController < ApplicationController
   def create_types(pokemon, types)
     types[1..].each { |t| PokemonType.create(pokemon: pokemon, type_id: t.to_i) }
   end
-  
+
   def create_attacks(pokemon, attacks)
     PokemonAttack.create(pokemon: pokemon, attack_id: attacks[0].to_i) if attacks.length > 0
     PokemonAttack.create(pokemon: pokemon, attack_id: attacks[1].to_i) if attacks.length > 1
